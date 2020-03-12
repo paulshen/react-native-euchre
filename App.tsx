971d6d95c4a53b5c428088681974f33087db68fe
Paul Shen <paulshen0@gmail.com>
2020-03-12 12:51:40 -0700

    setup TypeScript

--- App.tsx
+++ App.tsx
@@ -6,10 +6,10 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import firebase from '@react-native-firebase/app';
+import firebase from "@react-native-firebase/app";
 
 // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
 //    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
@@ -19,13 +19,14 @@ import firebase from '@react-native-firebase/app';
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu"
 });
 
 const firebaseCredentials = Platform.select({
-  ios: 'https://invertase.link/firebase-ios',
-  android: 'https://invertase.link/firebase-android',
+  ios: "https://invertase.link/firebase-ios",
+  android: "https://invertase.link/firebase-android"
 });
 
 type Props = {};
@@ -50,18 +51,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
