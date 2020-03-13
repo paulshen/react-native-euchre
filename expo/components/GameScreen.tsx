8e1d6aeaa2c3a462a675cf2cf6d1ffd4e00ba976
Paul Shen <paulshen0@gmail.com>
2020-03-13 14:00:10 -0700

    PlayerSwitcher toggle

--- expo/components/GameScreen.tsx
+++ expo/components/GameScreen.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   Picker,
   StyleSheet,
+  TouchableOpacity,
   View
 } from "react-native";
 import { Game } from "../game/Game";
@@ -14,10 +15,33 @@ import EnterGameScreen from "./EnterGameScreen";
 import { GameIdContext } from "./ReactContext";
 import WaitingRoomScreen from "./WaitingRoomScreen";
 
+function PlayerSwitcher({
+  playerNames,
+  player,
+  setPlayer
+}: {
+  playerNames: { [player: number]: string } | undefined;
+  player: Player;
+  setPlayer: (player: Player) => void;
+}) {
+  return (
+    <Picker
+      selectedValue={player}
+      onValueChange={itemValue => setPlayer(itemValue)}
+    >
+      <Picker.Item label="Player One" value={Player.One} />
+      <Picker.Item label="Player Two" value={Player.Two} />
+      <Picker.Item label="Player Three" value={Player.Three} />
+      <Picker.Item label="Player Four" value={Player.Four} />
+    </Picker>
+  );
+}
+
 export default function GameScreen() {
   const [gameId, setGameId] = React.useState<string | undefined>();
   const [player, setPlayer] = React.useState<Player | undefined>();
   const [game, setGame] = React.useState<Game | undefined>();
+  const [showPlayerSwitcher, setShowPlayerSwitcher] = React.useState(false);
 
   React.useEffect(() => {
     if (gameId !== undefined) {
@@ -71,15 +95,17 @@ export default function GameScreen() {
             setPlayer(undefined);
           }}
         />
-        <Picker
-          selectedValue={player}
-          onValueChange={itemValue => setPlayer(itemValue)}
-        >
-          <Picker.Item label="Player One" value={Player.One} />
-          <Picker.Item label="Player Two" value={Player.Two} />
-          <Picker.Item label="Player Three" value={Player.Three} />
-          <Picker.Item label="Player Four" value={Player.Four} />
-        </Picker>
+        <TouchableOpacity
+          style={styles.togglePlayerSwitcher}
+          onLongPress={() => setShowPlayerSwitcher(show => !show)}
+        />
+        {showPlayerSwitcher ? (
+          <PlayerSwitcher
+            playerNames={game?.playerNames}
+            player={player}
+            setPlayer={setPlayer}
+          />
+        ) : null}
       </View>
     </GameIdContext.Provider>
   );
@@ -92,5 +118,13 @@ const styles = StyleSheet.create({
   loading: {
     flex: 1,
     justifyContent: "center"
+  },
+  togglePlayerSwitcher: {
+    backgroundColor: "transparent",
+    height: 24,
+    width: 84,
+    position: "absolute",
+    right: 0,
+    top: 32
   }
 });
